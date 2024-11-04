6dc7faafa0b9697dfc48a6e896be40f74ad164bb
Tucker McKee <tuckermckee@Tuckers-MacBook-Pro.local>
2024-11-04 15:23:17 -0600

    init commit

---
+++ makeText.js
@@ -0,0 +1,42 @@
+/** Command-line tool to generate Markov text. */
+const markov = require('markov.js');
+const axios = require('axios');
+const fs = require('fs');
+
+async function getUrlString(url) {
+    try {
+        const res = await axios.get(url);
+        return res.data
+    } catch (error) {
+        console.error("Error fetching URL:", error);
+        return null;
+    }
+}
+
+
+
+async function makeText() {
+    let text;
+    let mm;
+    switch(process.argv[2]) {
+        case 'file':
+            text = fs.readFile(process.argv[3],'utf-8', (err,data) => {
+                if (err) {
+                   console.log(err);
+                   process.kill(1); 
+                }
+                return data
+            });
+            mm = markov.MarkovMachine(text);
+            mm.makeText();
+            break;
+        case 'url':
+            text = await getUrlString(process.argv[3]);
+            mm = markov.MarkovMachine(text);
+            mm.makeText();
+            break;    
+    }
+    
+}
+
+makeText();
---
+++ markov.js
@@ -0,0 +1,67 @@
+/** Textual markov chain generator */
+
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter(c => c !== "");
+    this.chains = this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    let chains = {};
+    const uniqueWords = [...new Set(this.words)]; //remove duplicate words
+    uniqueWords.forEach( val => {
+      let chainWords = [];
+
+      this.words.forEach( (val2,idx) => { 
+        if (this.words[idx - 1] == val && idx > 0) {
+          chainWords.push(val2);
+        }
+      })
+
+      chains[val] = chainWords;
+    })
+    for (let key in chains) { //if word has no chain options, set to null
+      if (chains[key][0] === undefined){
+        chains[key][0] = null;
+      }
+    }
+    return chains
+  }
+
+
+  /** return random text from chains */
+
+  makeText(numWords = 100) {
+    const chains = this.chains;
+    let firstWord = this.words[Math.floor(Math.random() * this.words.length)];
+    while (!chains[firstWord][0]) {
+      firstWord = this.words[Math.floor(Math.random() * this.words.length)];
+    } // ensuring first word of text has options for chaining
+    let currWord;
+    let nextWord;
+    let text = [firstWord];
+    for (let x = 0; x < numWords; x++) {
+      if (x == 0) {
+        currWord = firstWord;
+      }
+      nextWord = chains[currWord][Math.floor(Math.random() * chains[currWord].length)];
+      while (x + 1 < numWords && !chains[nextWord][0]) {
+        nextWord = chains[currWord][Math.floor(Math.random() * chains[currWord].length)];
+      } // ensuring next word has chaining options if not last word
+      text.push(nextWord);
+      currWord = nextWord;
+    }
+    console.log(text.join(' '));
+    return 
+  }
+}
